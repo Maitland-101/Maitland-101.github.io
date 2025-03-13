ae186f6506b42a752f777994a0b01884148865b9
Maitland-101 <raydar19@icloud.com>
2025-03-13 10:26:00 -0600

    average line

--- Terrain Generation Project/sketch.js
+++ Terrain Generation Project/sketch.js
@@ -7,7 +7,7 @@ let noiseTime = 5;
 let noiseStart = 5;
 let noiseSpeed = 0.01;
 let rectWidth = 30;
-let peak = 0;
+let peak;
 let tallX;
 
 function setup() {
@@ -20,11 +20,16 @@ function draw() {
   noiseStart += noiseSpeed; // count time from start of program
   generateTerrain();
   drawFlag(tallX,height-peak);
+  averageLine(height-peak/2);
 }
 
 function generateTerrain(){
   //use a loop to generate and draw rectangles to look like 2D terrain
   rectMode(CORNERS);
+  fill('grey');
+  //resets x,y of flag each frame
+  peak = 0;
+  tallX = 0;
   for(x1 = 0; x1<width; x1+= rectWidth){
     //generate a random height change this from using random() to noise()
     let rectHeight = noise(noiseTime);
@@ -47,10 +52,10 @@ function generateTerrain(){
 
 function keyPressed(){
   // when an arrow key is pressed change the width of the rectangle
-  if(keyCode === 39){
+  if(keyCode === 39 && rectWidth<=50){
     rectWidth = rectWidth+5;
   }
-  if(keyCode === 37 && rectWidth>15){
+  if(keyCode === 37 && rectWidth>=6){
     rectWidth = rectWidth-5;
   }
 }
@@ -60,4 +65,10 @@ function drawFlag(x,y){
   fill('grey');
   rect(x+rectWidth/2,y-25,5,25);
   triangle(x+rectWidth/2, y-35, x+rectWidth/2+15, y-25, x+rectWidth/2, y-15);
+}
+
+function averageLine(y){
+  //draws a line at the average height of the rectangles
+  fill('red');
+  rect(0,y,width,5);
 }
