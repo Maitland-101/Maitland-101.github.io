eca3c6af80b735a0e884df631836e279821dd061
Maitland-101 <raydar19@icloud.com>
2025-03-17 12:18:00 -0600

    Round Racers challenge

---
+++ Create First Class/sketch.js
@@ -0,0 +1,48 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let racer1;
+let racerCars = [];
+const NUM_RACERS = 3;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  racer1 = new RoundRacer(width/2, "green");
+}
+
+function draw() {
+  background(255);
+  racer1.display();
+  racer1.move();
+  //racers();
+  for(i=0; i<NUM_RACERS; i++){ 
+    racer1.move();
+    racer1.display();
+  }
+  
+}
+
+class RoundRacer{
+  //1. constructor
+  constructor(yPostion, color){
+    this.xPostion = 0;
+    this.yPostion = yPostion;
+    this.xSpeed = round(random(3,15));
+    this.color = color;
+  }
+  //2. methods
+  move(){
+    this.xPostion += this.xSpeed;
+    if (this.xPostion>width){
+      this.xPostion = 0;
+    }
+  }
+  display(){
+    fill(this.color);
+    circle(this.xPostion, this.yPostion,5);
+  }
+}
