b5d10c03fac6929037d660353d2e4d6d81d92cdf
Maitland-101 <raydar19@icloud.com>
2025-03-21 12:17:09 -0600

    car and road made

---
+++ Cars Cars Cars/sketch.js
@@ -0,0 +1,78 @@
+// Cars Cars Cars
+// Alyssa Maitland
+// Mar.21, 2025
+
+//Global Variables
+let car;
+let carWidth = 50;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  car = new Vehicle(0,"west");
+}
+
+function draw() {
+  background(220);
+  drawRoad();
+  car.action();
+}
+
+function drawRoad(){
+  //draws rectangles that make up the road
+  fill(0);
+  rect(0, height*0.2, width, height*0.6);
+  for(i = 0; i < width; i+=40){
+    // draws a dotted line in the road for the width of the screen
+    fill("yellow");
+    rect(i, height/2, 20, 5);
+  }
+}
+
+class Vehicle{
+  //1.consrutor
+  constructor(type, direction){
+    this.carType = type;
+    this.c = color(random(255), random(255), random(255));
+    this.x = random(width);
+    this.y = random(height*0.2, height*0.6);
+    this.direction = direction;
+    this.carSpeed = 10;
+  }
+  //2. class methods
+  display(){
+    //draw a car/truck at the x,y of the direction
+    fill(this.c);
+    if(this.carType === 0){
+      if(this.direction === "west"){
+        rect(this.x, this.y/2, carWidth);
+      }
+      else if(this.y<height/2){
+        circle(this.x, this.y, carWidth);
+      }
+    }
+    if(this.carType === 1){
+      rect(this.x, this.y, carWidth);
+    }
+  }
+
+  move(){
+    this.x += this.carSpeed;
+    if(this.x>width){
+      this.x = -carWidth;
+    }
+  }
+
+  speedUp(){
+  }
+
+  speedDown(){
+  }
+
+  changeColor(){
+  }
+
+  action(){
+    this.display();
+    this.move();
+  }
+}
