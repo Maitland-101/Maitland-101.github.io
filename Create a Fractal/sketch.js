7c67f0e7324ae29eb732d553c9f1b17de6354892
Maitland-101 <raydar19@icloud.com>
2025-04-29 12:16:50 -0600

    one line of triangle done

--- Create a Fractal/sketch.js
+++ Create a Fractal/sketch.js
@@ -5,11 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
 }
 
 function draw() {
   background(220);
+  triangleFractal(width/2,height/2,250);
+}
+
+function triangleFractal(x1,y1,triWidth){
+  triWidth = triWidth/2;
+  triangle(x1-triWidth,y1+triWidth,x1,y1-triWidth,x1+triWidth,y1+triWidth);
+  if(triWidth>0){
+    triangleFractal(x1-triWidth/2,y1+triWidth/2,x1,y1-triWidth/2,x1+triWidth/2,y1+triWidth/2);
+    triangleFractal(x1+triWidth*2,y1+triWidth*2,x1,y1-triWidth*2,x1-triWidth*2,y1+triWidth*2);
+  }
 }
