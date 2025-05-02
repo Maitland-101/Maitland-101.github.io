0d6e77c566d22d140480906a304e54e418ca5859
Maitland-101 <raydar19@icloud.com>
2025-05-02 10:45:23 -0600

    Fixed program made added no stroke and random colours

--- Create a Fractal/sketch.js
+++ Create a Fractal/sketch.js
@@ -1,9 +1,7 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Create a Fractal
+// Alyssa Maitland
+// Apr.2, 2025
+// create a triangle with with triangles inside that half their size and move to each corner generated recursivly
 
 function setup() {
   createCanvas(500, 500);
@@ -11,14 +9,19 @@ function setup() {
 
 function draw() {
   background(220);
-  triangleFractal(width/2,height/2,250);
+  randomSeed(1);
+  triangleFractal(250,250,250);
 }
 
 function triangleFractal(x1,y1,triWidth){
-  triWidth = triWidth/2;
+  noStroke();
+  fill(random(255),random(255),random(255),random(200));
   triangle(x1-triWidth,y1+triWidth,x1,y1-triWidth,x1+triWidth,y1+triWidth);
-  if(triWidth>0){
-    triangleFractal(x1-triWidth/2,y1+triWidth/2,x1,y1-triWidth/2,x1+triWidth/2,y1+triWidth/2);
-    triangleFractal(x1+triWidth*2,y1+triWidth*2,x1,y1-triWidth*2,x1-triWidth*2,y1+triWidth*2);
+  
+  if(triWidth>10){
+    triangleFractal(x1-triWidth/2,y1+triWidth/2,triWidth/2); //bottom left
+    triangleFractal(x1+triWidth/2,y1+triWidth/2,triWidth/2); //bottom right
+    triangleFractal(x1,y1-triWidth/2,triWidth/2); //top
   }
+  //base case: if triWidth is <11 then do nothing
 }
