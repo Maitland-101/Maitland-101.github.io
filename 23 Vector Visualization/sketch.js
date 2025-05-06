3b489a6b8cafd7aa8d9a130a865c7d9b3c677033
Maitland-101 <raydar19@icloud.com>
2025-05-06 11:06:09 -0600

    Finished

--- 23 Vector Visualization/sketch.js
+++ 23 Vector Visualization/sketch.js
@@ -1,6 +1,6 @@
 // Vector Visualization
 // Alyssa M.
-// May.5, 2025
+// May 5, 2025
 
 let objects = [];
 
@@ -15,61 +15,64 @@ function draw() {
   if(mouseIsPressed){
     objects.push(new Ball(mouseX, mouseY));
   }
-  for(let o of objects){
+
+  for (let o of objects){
     //if(keyIsPressed && key === " "){
       o.move();
     //}
     //o.calcMouse();
     o.display();
   }
+
 }
 
 class Ball{
   constructor(x,y){
     this.pos = createVector(x,y);
-    this.velo = createVector(random(5,-5),-5);
+    this.vel = createVector(random(-5,5),-5);
     this.grav = createVector(0,0.2);
-    //this.grav = createVector(0,0); //mouse attractor
+    //this.grav; //MOUSE ATTRACTOR
   }
 
   calcMouse(){
     //mouse vector calculations
-    this.grav = createVector(mouseX,mouseY);
+    this.grav = createVector(mouseX, mouseY);
     this.grav.sub(this.pos);
-    this.grav.normalize();
+    this.grav.normalize(); //sets between 0-1
     this.grav.mult(4);
   }
 
   move(){
-    //update velo and pois vectors
-    this.velo.add(this.grav);
-    this.velo.limit(10); 
-    this.pos.add(this.velo);
+    //update velocity and position vectors
+    this.vel.add(this.grav);
+    this.vel.limit(30);  //can't go outside -10 and 10
+    this.pos.add(this.vel);
 
     //wall-floor bounce
-    if(this.pos.x<0 || this.pos.x>width){
-      this.velo.x *= -1;
+    if(this.pos.x < 0 || this.pos.x > width){
+      this.vel.x *= -1;
     }
-    if(this.pos.y<height){
-      this.velo.y *= -1;
+    if(this.pos.y > height){
+      this.vel.y *= -1;
     }
   }
-
+  
   display(){
-    //diaplay ball
+    //display Ball
     circle(this.pos.x, this.pos.y, 20);
 
     //display vectors
-    stroke(2550,0,0);
-    line(0,0,this.pos.x,this.pos.y);
+    // stroke(255,0,0);
+    // line(0,0,this.pos.x, this.pos.y);
 
-    stroke(0,0,255);
-    line(this.pos.x,this.pos.y, this.pos.x + this.velo.x, this.pos.y + this.velo.y);
+    // stroke(0,0,255);
+    // line(this.pos.x, this.pos.y, this.pos.x + this.vel.x, this.pos.y + this.vel.y);
 
-    let endVX = this.pos.x+this.velo.x;
-    let endVY = this.pos.y+this.velo.y;
-    stroke(0,255,0);
-    line(endVX, endVY, endVX + this.grav.x, endVY+this.grav.y);
+    // let endVX = this.pos.x + this.vel.x;
+    // let endVY = this.pos.y + this.vel.y;
+    // stroke(0,255,0);
+    // line(endVX, endVY, endVX + this.grav.x, endVY + this.grav.y);
 
   }
+
 }
