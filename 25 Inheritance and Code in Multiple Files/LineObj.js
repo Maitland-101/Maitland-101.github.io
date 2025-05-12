7935b6c8e74a12f0569ed97e07a2a9e8cab9edfe
Maitland-101 <raydar19@icloud.com>
2025-05-12 12:07:31 -0600

    Demo Finished

---
+++ 25 Inheritance and Code in Multiple Files/LineObj.js
@@ -0,0 +1,26 @@
+//child class #2  - Line
+class LineObj extends AnimatedObject{
+    constructor(){
+      super(random(width), random(height));
+    }
+  
+    move(){
+      //combo override + build on parent class version
+      super.move(); //begins by running parent class move()
+      this.x -= 5;
+      if(this.x < 0){
+        this.x = width;
+      }
+    }
+  
+    display(){
+      //full overide
+      if(mouseIsPressed){
+        strokeWeight(12);
+      }
+      else{
+        strokeWeight(2);
+      }
+      line(this.x, this.y, this.x+15, this.y);
+    }
+  }
