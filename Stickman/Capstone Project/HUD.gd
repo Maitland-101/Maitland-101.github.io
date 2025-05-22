7f06f3dbe60da6ca585b4a1f3ea5df4e6d550232
Maitland-101 <raydar19@icloud.com>
2025-05-22 10:59:43 -0600

    finished Hud displaay

--- Stickman/Capstone Project/HUD.gd
+++ Stickman/Capstone Project/HUD.gd
@@ -22,3 +22,16 @@ func show_game_over():
 	
 	$Message.text = 'dodge the projectiles'
 	$Message.show()
+	
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_message_timer_timeout():
+	$Message.hide()
--- Stickman/Capstone Project/level.gd
+++ Stickman/Capstone Project/level.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,18 +17,25 @@ func game_over():
 	#stop timers when player is hit
 	$ScoreTimer.stop()
 	$ProjectileTimer.stop()
+	
+	$HUD.show_game_over()
 
 func new_game():
 	#when a new game is started set player back to spawn and start the timers
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("get ready")
+	
+	get_tree().call_group("projectiles", "quene_free")
 
 func _on_projectile_timer_timeout():
 	#creates projectiles every 0.5 seconds until player is hit
 	var projectile = projectile_scene.instantiate()
 	#random spawn location
-	var projectile_spawn_location = $MobPath/MobSpawnLocation
+	var projectile_spawn_location = $ProjectilePath/ProjectileSpawnLocation
 	projectile_spawn_location.progress_ratio = randf()
 	projectile.position = projectile_spawn_location.position
 	#set direction
@@ -38,11 +45,14 @@ func _on_projectile_timer_timeout():
 	var velocity = Vector2(250, 0.0)
 	projectile.linear_velocity = velocity.rotated(direction)
 	#add projectile
+	projectile.add_to_group('projectiles')
 	add_child(projectile)
 
 func _on_score_timer_timeout():
 	#add point to score for every second survived
 	score += 1
+	
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
