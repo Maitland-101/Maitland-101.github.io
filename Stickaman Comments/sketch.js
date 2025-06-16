bef6c4e246c5c9ddaf860d908a7c00dba990cfb7
Maitland-101 <raydar19@icloud.com>
2025-06-16 10:45:34 -0600

    Comments

---
+++ Stickaman Comments/sketch.js
@@ -0,0 +1,33 @@
+// Project TitleRRRR
+// Your Name
+
+// Date
+//16th June 2025
+//COMMENTS
+//Priyansh Jhanji
+// Nice try! But there are a couple of bugs, When you fall you keep falling and after level 2 on the fag the player never progresses to another level. Nice Try! :)
+
+//Maheen
+//the game is well made. At one point I wasn'tsure how to reset it but otherwise great job!
+
+//IF GAME OVER CLOSE AND CLICK RUN IN GODOT EDITOR
+
+//Gary xinchen: this game is a good idea but there is a few bugs when the stick man is falling, but other elements are actually crazy!!
+
+//Fahad Hussain: the game is pretty good and id somewhat fun however
+// animations could use some smoothing sync with motion, menu could have more options, there could be more levels and also u can debug the player and the projectiles
+//as i didnt see a projectile to dodge
+
+/* Marcus: The goal was clear to make a platformer but
+There was no true goal as my "Highscore" kept going up
+Meaning I was winning without having to try
+Very simple but I could see there was more meant to be added and fixed
+
+*/
+
+//Game was simple, but the level desing wasnt that good 
+//with te platforms not matching the level well.
+//The character was also very floaty
+//Fall animations would have been a good add
+
+// I like the game mechanics, but you can't respwan after death in lvl 2
